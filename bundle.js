294c1f933323639e1f41078ed2264492c28c9f17
dollartoken <dollartoken@protonmail.com>
2017-07-19 17:10:14 +0300

    Apply last changes

--- bundle.js
+++ bundle.js
@@ -18928,91 +18928,87 @@ var BigNumber = __webpack_require__(3);
 
 console.log('%cYou can do nothing here. Go away.', 'background: red; color: yellow; font-size: x-large');
 
-function decodeStats(response, price) {
-    if (response == null) return null;
+// function decodeStats(response, price) {
+//     if (response == null) return null;
 
-    var result = response.result;
-    if (result == null || result.length == null || result.length < 193) return null;
+//     var result = response.result;
+//     if (result == null || result.length == null || result.length < 193) return null;
 
-    var weiPerEther = new BigNumber('1000000000000000000', 10);
+//     var weiPerEther = new BigNumber('1000000000000000000', 10);
 
-    var totalContributionExact = new BigNumber(result.substr(2, 64), 16).div(weiPerEther);
-    var totalContributionUSDExact = totalContributionExact.times(new BigNumber(price));
+//     var totalContributionExact = new BigNumber(result.substr(2, 64), 16).div(weiPerEther);
+//     var totalContributionUSDExact = totalContributionExact.times(new BigNumber(price));
 
-    return {
-        totalContribution: totalContributionExact.round(3, BigNumber.ROUND_DOWN),
-        totalContributionUSD: totalContributionUSDExact.round(0, BigNumber.ROUND_DOWN),
-        totalContributionTVs: totalContributionUSDExact.div(new BigNumber("1200")).round(0, BigNumber.ROUND_DOWN),
-        totalSupply: new BigNumber(result.substr(66, 64), 16).div(weiPerEther).round(3, BigNumber.ROUND_DOWN),
-        totalBonusTokensIssued: new BigNumber(result.substr(130, 64), 16).div(weiPerEther).round(3, BigNumber.ROUND_DOWN),
-        purchasingAllowed: new BigNumber(result.substr(194, 64), 16).isZero() == false
-    };
-}
+//     return {
+//         totalContribution: totalContributionExact.round(3, BigNumber.ROUND_DOWN),
+//         totalContributionUSD: totalContributionUSDExact.round(0, BigNumber.ROUND_DOWN),
+//         totalContributionTVs: totalContributionUSDExact.div(new BigNumber("1200")).round(0, BigNumber.ROUND_DOWN),
+//         totalSupply: new BigNumber(result.substr(66, 64), 16).div(weiPerEther).round(3, BigNumber.ROUND_DOWN),
+//         totalBonusTokensIssued: new BigNumber(result.substr(130, 64), 16).div(weiPerEther).round(3, BigNumber.ROUND_DOWN),
+//         purchasingAllowed: new BigNumber(result.substr(194, 64), 16).isZero() == false
+//     };
+// }
 
-function getStats(price) {
-    var url = 'https://api.etherscan.io/api?module=proxy&action=eth_call&to=0x27f706edde3aD952EF647Dd67E24e38CD0803DD6&data=0xc59d48470000000000000000000000000000000000000000000000000000000000000000&tag=latest';
-    return $.ajax(url, {
-        cache: false,
-        dataType: 'json'
-    }).then(function (data) {
-        return decodeStats(data, price);
-    });
-}
+// function getStats(price) {
+//     var url = 'https://api.etherscan.io/api?module=proxy&action=eth_call&to=0x27f706edde3aD952EF647Dd67E24e38CD0803DD6&data=0xc59d48470000000000000000000000000000000000000000000000000000000000000000&tag=latest';
+//     return $.ajax(url, {
+//         cache: false,
+//         dataType: 'json'
+//     }).then(function (data) { return decodeStats(data, price); });
+// }
 
-function getPrice() {
-    var url = 'https://api.etherscan.io/api?module=stats&action=ethprice';
-    return $.ajax(url, {
-        cache: false,
-        dataType: 'json'
-    }).then(function (data) {
-        if (data == null) return null;
-        if (data.result == null) return null;
-        if (data.result.ethusd == null) return null;
+// function getPrice() {
+//     var url = 'https://api.etherscan.io/api?module=stats&action=ethprice';
+//     return $.ajax(url, {
+//         cache: false,
+//         dataType: 'json'
+//     }).then(function (data) {
+//         if (data == null) return null;
+//         if (data.result == null) return null;
+//         if (data.result.ethusd == null) return null;
 
-        return parseFloat(data.result.ethusd);
-    });
-}
+//         return parseFloat(data.result.ethusd);
+//     });
+// }
 
-function updatePage(stats) {
-    if (stats == null) return;
-    if (!stats.purchasingAllowed /* && Date.now < Date.UTC(2017, 6, 4, 14)*/) {
-            $('#stats').hide();
-            return;
-        }
+// function updatePage(stats) {
+//     if (stats == null) return;
+//     if (!stats.purchasingAllowed/* && Date.now < Date.UTC(2017, 6, 4, 14)*/) {
+//         $('#stats').hide();
+//         return;
+//     }
 
-    $('#total-ether').text(stats.totalContribution.toFixed(3));
-    if (stats.totalContribution.toNumber() <= 0) {
-        $('#total-ether-message').text('Looks like everyone read the warnings so far.');
-    } else {
-        $('#total-ether-message').text('I had a feeling someone would waste their money.');
-    }
+//     $('#total-ether').text(stats.totalContribution.toFixed(3));
+//     if (stats.totalContribution.toNumber() <= 0) {
+//         $('#total-ether-message').text('Looks like everyone read the warnings so far.');
+//     } else {
+//         $('#total-ether-message').text('I had a feeling someone would waste their money.');
+//     }
 
-    $('#total-usd').text('$' + stats.totalContributionUSD.toFixed(0));
-    if (stats.totalContributionUSD.toNumber() <= 0) {
-        $('#total-usd-message').text('No Ether yet, so no cash either.');
-    } else if (stats.totalContributionTVs.toNumber() < 1) {
-        $('#total-usd-message').text('Not enough to buy a television yet.');
-    } else if (stats.totalContributionTVs.toNumber() < 2) {
-        $('#total-usd-message').text('Enough to buy a television.');
-    } else {
-        $('#total-usd-message').text('Enough to buy ' + stats.totalContributionTVs.toFixed(0) + ' televisions!');
-    }
+//     $('#total-usd').text('$' + stats.totalContributionUSD.toFixed(0));
+//     if (stats.totalContributionUSD.toNumber() <= 0) {
+//         $('#total-usd-message').text('No Ether yet, so no cash either.');
+//     } else if (stats.totalContributionTVs.toNumber() < 1) {
+//         $('#total-usd-message').text('Not enough to buy a television yet.');
+//     }else if (stats.totalContributionTVs.toNumber() < 2) {
+//         $('#total-usd-message').text('Enough to buy a television.');
+//     } else {
+//         $('#total-usd-message').text('Enough to buy ' + stats.totalContributionTVs.toFixed(0) + ' televisions!');
+//     }
 
-    $('#total-tokens').text(stats.totalSupply.toFixed(3));
-    if (stats.totalSupply <= 0) {
-        $('#total-tokens-message').text('No useless tokens issued yet either.');
-    } else if (stats.totalBonusTokensIssued.toNumber() <= 0) {
-        $('#total-tokens-message').text('Look at all those useless tokens!');
-    } else {
-        $('#total-tokens-message').text('Including ' + stats.totalBonusTokensIssued.toFixed(3) + ' bonus tokens!');
-    }
+//     $('#total-tokens').text(stats.totalSupply.toFixed(3));
+//     if (stats.totalSupply <= 0) {
+//         $('#total-tokens-message').text('No useless tokens issued yet either.');
+//     } else if (stats.totalBonusTokensIssued.toNumber() <= 0) {
+//         $('#total-tokens-message').text('Look at all those useless tokens!');
+//     } else {
+//         $('#total-tokens-message').text('Including ' + stats.totalBonusTokensIssued.toFixed(3) + ' bonus tokens!');
+//     }
 
-    $('#stats').show();
-}
+//     $('#stats').show();
+// }
 
-function refresh() {
-    getPrice().then(getStats).then(updatePage);
-}
+// function refresh() { getPrice().then(getStats).then(updatePage); }
 
 function startCountdown() {
     $('.countdown').countdown('2017/07/27', function (event) {
@@ -19030,8 +19026,8 @@ function fillFaces() {
 
 $(function () {
     try {
-        refresh();
-        setInterval(refresh, 1000 * 60 * 5);
+        // refresh();
+        // setInterval(refresh, 1000 * 60 * 5);
         startCountdown();
 
         new Clipboard('#eth-address-copy-button');
